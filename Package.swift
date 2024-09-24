9cf24d64b39219d66985414f6210a14074cafb48
Kit Transue <src@kittransue.net>
2024-09-23 17:19:24 -0700

    Require deft-log 1.1.0 for Swift 6 support

--- Package.swift
+++ Package.swift
@@ -13,13 +13,9 @@ let package = Package(
         .library(name: "OBSAsyncAPI", targets: ["OBSAsyncAPI"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/didactek/deft-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/didactek/deft-log.git", from: "1.1.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "SampleClient",
             dependencies: [
